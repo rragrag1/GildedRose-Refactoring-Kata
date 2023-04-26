911007511d623e1295edb25ed529703486b1147e
RAGRAG Radouan <r.ragrag@hotmail.com>
2023-04-26 18:19:56 +0100

    refactor conditional name section : isolate and move up every category

--- Java-Cucumber/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Cucumber/src/main/java/com/gildedrose/GildedRose.java
@@ -12,16 +12,16 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (!item.name.equals(AGED_BRIE)  && !item.name.equals(BACKSTAGE)) {
-                if (!item.name.equals(SULFURAS)) {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-            }
             if (item.name.equals(AGED_BRIE)) {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
+                }
+            }
+            if (item.name.equals(AGED_BRIE)) {
+                if (item.sellIn < 0) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
                 }
             }
             if (item.name.equals(BACKSTAGE)) {
@@ -41,26 +41,27 @@ class GildedRose {
                     }
                 }
             }
-
             if (!item.name.equals(SULFURAS)) {
                 item.sellIn = item.sellIn - 1;
             }
-
-            if (item.sellIn < 0) {
-                if (!item.name.equals(AGED_BRIE)) {
-                    if (!item.name.equals(BACKSTAGE) && !item.name.equals(SULFURAS)) {
-                        if (item.quality > 0) {
-                            item.quality = item.quality - 1;
-                        }
-                    }
-
-                    if (item.name.equals(BACKSTAGE)) {
-                        item.quality = 0;
-                    }
+            if (item.name.equals(BACKSTAGE)) {
+                if (item.sellIn < 0) {
+                    item.quality = 0;
                 }
-                if (item.name.equals(AGED_BRIE)) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
+            }
+
+
+            if (!item.name.equals(AGED_BRIE) && !item.name.equals(BACKSTAGE) && !item.name.equals(SULFURAS)) {
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
+                }
+            }
+
+
+            if (!item.name.equals(AGED_BRIE) && !item.name.equals(BACKSTAGE) && !item.name.equals(SULFURAS)) {
+                if (item.sellIn < 0) {
+                    if (item.quality > 0) {
+                        item.quality = item.quality - 1;
                     }
                 }
             }

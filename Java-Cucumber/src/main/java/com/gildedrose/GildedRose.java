6e561a668554f8707b288618bc9276020a8fcbea
RAGRAG Radouan <r.ragrag@hotmail.com>
2023-04-26 18:29:10 +0100

    reorganize conditional name section in order to merge them

--- Java-Cucumber/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Cucumber/src/main/java/com/gildedrose/GildedRose.java
@@ -17,7 +17,9 @@ class GildedRose {
                     item.quality = item.quality + 1;
                 }
             }
-
+            if (item.name.equals(AGED_BRIE)) {
+                item.sellIn = item.sellIn - 1;
+            }
             if (item.name.equals(AGED_BRIE)) {
                 if (item.sellIn < 0) {
                     if (item.quality < 50) {
@@ -43,22 +45,19 @@ class GildedRose {
                     }
                 }
             }
-
-            if (item.name.equals(SULFURAS)) {
-            }
-            if (item.name.equals(AGED_BRIE)) {
-                item.sellIn = item.sellIn - 1;
-            }
-            if (item.name.equals(BACKSTAGE)) {
-                item.sellIn = item.sellIn - 1;
-            }
-            if (!item.name.equals(AGED_BRIE) && !item.name.equals(BACKSTAGE) && !item.name.equals(SULFURAS)) {
-                item.sellIn = item.sellIn - 1;
-            }
             if (item.name.equals(BACKSTAGE)) {
                 if (item.sellIn < 0) {
                     item.quality = 0;
                 }
+            }
+            if (item.name.equals(BACKSTAGE)) {
+                item.sellIn = item.sellIn - 1;
+            }
+
+            if (item.name.equals(SULFURAS)) {
+            }
+            if (!item.name.equals(AGED_BRIE) && !item.name.equals(BACKSTAGE) && !item.name.equals(SULFURAS)) {
+                item.sellIn = item.sellIn - 1;
             }
 
             if (!item.name.equals(AGED_BRIE) && !item.name.equals(BACKSTAGE) && !item.name.equals(SULFURAS)) {
